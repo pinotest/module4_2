2d9b496b6db32a8a4e77ffdd4c893f42802c2a81
Piotr Nowak <piotr.cezary.nowak@gmail.com>
2020-05-31 20:12:24 +0200

    Change functions multiply and add numbers for many numbers

--- calculator.py
+++ calculator.py
@@ -1,18 +1,26 @@
 import logging
-#logging.basicConfig(level=logging.INFO,format='%(asctime)s %(message)s')
+logging.basicConfig(level=logging.INFO)#,format='%(asctime)s %(message)s')
 
-def add_numbers(first, second):
-    logging.info("Dodaję %s + %s" %(first, second))
-    return first + second
+def add_numbers(user_list_numbers):
+    user_numbers_sum = 0
+    for i, num_to_add in enumerate(user_list_numbers):
+        if i > 0:
+            logging.info("Dodaję %s + %s" %(user_numbers_sum, user_list_numbers[i]))
+        user_numbers_sum += num_to_add
+    return user_numbers_sum
 
 def odd_numbers(first,second):
     logging.info("Odejmuję %s - %s" %(first, second))
     return first-second
 
-def multiply_numbers(first,second):
-    logging.info("Mnożę  %s * %s" %(first, second))
-    return first*second
-
+def multiply_numbers(user_list_numbers):
+    user_numbers_multi = 1
+    for i, num_to_multi in enumerate(user_list_numbers):
+        if i > 0:
+            logging.info("Mnożę %s * %s" %(user_numbers_multi , user_list_numbers[i]))
+        user_numbers_multi *= num_to_multi
+    return user_numbers_multi
+ 
 def divide_numbers(first, second):
     logging.info("Dzielę  %s / %s" %(first, second))
     return first/second
@@ -31,7 +39,16 @@ def get_numbers():
         except ValueError:
             print("Wprowadź poprawną wartość liczbową!")
     return first_number_choice,second_number_choice
-
+def get_list_numbers():
+    user_list_numbers = []
+    while not user_list_numbers:
+        try:
+            user_list_numbers = (input("Podaj listę liczb oddzielając je przecinkiem ").split(","))
+            user_list_numbers = [float(i) for i in user_list_numbers]
+        except:
+            print("Wprowadź poprawne wartość liczbową!")
+        return user_list_numbers
+    
 choice = ''
 while not choice:
     try: 
@@ -39,14 +56,14 @@ while not choice:
     except ValueError:
         print("Wprowadź poprawną wartość liczbową!")
     if choice == 1:
-        first_number, second_number = get_numbers()
-        print("Wynik to: ", add_numbers(first_number,second_number))
+        user_numbers = get_list_numbers()
+        print("Wynik to: ", add_numbers(user_numbers))
     elif choice ==2:
         first_number, second_number = get_numbers()
         print("Wynik to: ", odd_numbers(first_number,second_number))
     elif choice ==3:
-        first_number, second_number = get_numbers()
-        print("Wynik to: ", multiply_numbers(first_number,second_number))
+        user_numbers = get_list_numbers()
+        print("Wynik to: ", multiply_numbers(user_numbers))
     elif choice ==4:
         first_number, second_number = get_numbers()
         while second_number == 0:
